0520d5287045aedc592e7bbf9d297d6024465e0c
ajnanmvr <ajnanpulikkathody@gmail.com>
2021-11-18 20:02:35 +0530

    Node.js file Updated

--- node.js
+++ node.js
@@ -1,34 +1,40 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
+const url = require('url')
+const fs  = require('fs')
+const http = require('http')
 
-http.createServer(function  (req,res) {
+http.createServer( (req,res)=> {
+    const myUrl = url.parse(req.url,true)
 
-    var q = url.parse(req.url,true)
-    //q.pathname
+    if(myUrl.pathname==="/"){
+        fs.readFile('index.html',(err,data)=>{
 
-    if(q.pathname==="/"){
-        fs.readFile('index.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
-        res.end()})
+            res.write(data)
+            res.end()
+
+        })
+    } else if (myUrl.pathname === '/login') {
+        fs.readFile('login.html',(err,data)=>{
+
+            res.writeHead(200,{'content-type':'text/html'})
+            res.write(data)
+            res.end()
+
+
+        })  
+    } else if (myUrl.pathname==='/loginaction'){
+        fs.readFile('logged.html',(err,data)=>{
+
+            res.writeHead(200,{'content-type':'text/html'})
+            res.write(data)
+            console.log("|  => "+myUrl.query.uname + " : " + myUrl.query.psw);
+            res.end()
+
+        })
+    } else {
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write("<h1> Error </h1>")
+        res.end()
     }
-    else if (q.pathname === "/login"){
-        fs.readFile('login.html', function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
-        res.end()})
-    }        
-    else if (q.pathname === "/loginaction") {
-        res.writeHead(404,{'Content-Type':'text/html'})
-        res.write("<h1>"+q.query.psw +"</h1>")
-        res.end()
-    } 
-                  else{
-        res.writeHead(404,{'Content-Type':'text/html'})
-        res.write("error")
-        res.end()
-    }       
 
-
-}).listen(7000,()=> console.log("prgm running"))
+}).listen(7000,()=>{console.log('Node Js Started running');console.log(' ');console.log("____________________");console.log("UserName : Password");console.log("____________________");})
